8254ab5a4a9d8e1b64b48ee8d2901724216e28e4
Matej Krebelj <matej.krebelj@gmail.com>
2014-07-30 14:03:02 +0200

    test

--- src/com/wifreemaps/PromptGPS.java
+++ src/com/wifreemaps/PromptGPS.java
@@ -22,7 +22,7 @@ public class PromptGPS extends DialogFragment {
                })
                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
-                       // User cancelled the dialog
+                       // User cancelled the dialog 
                    }
                });
         // Create the AlertDialog object and return it
